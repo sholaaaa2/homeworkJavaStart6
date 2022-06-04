741ed7c08ecae3c90c393a9293143258f0ab507e
sholaaaa2 <92998923+sholaaaa2@users.noreply.github.com>
2022-06-04 17:53:57 +0300

    Update HW1.1.java

--- HW1.1.java
+++ HW1.1.java
@@ -9,7 +9,7 @@ public class Main {
 	}
 
 	public static int getMax(int[] arr) {
-		int max = 0;
+		int max = arr[0];
 		for (int i = 0; i < arr.length; i++) {
 			max = (max < arr[i]) ? arr[i] : max;
 		}
